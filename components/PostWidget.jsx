552909316285aadd3400c5987a6c322b54dfc6e1
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-08-25 12:09:38 +0200

    PostWidget component - services, component and RelatedPost component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,5 +1,34 @@
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+import { useState, useEffect } from 'react';
+
+// Components
+import { RelatedPost } from '../components';
+
+// gql
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, [slug]);
+
+  return (
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <RelatedPost key={post.id} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
---
+++ components/RelatedPost.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import moment from 'moment';
+import Link from 'next/link';
+
+const RelatedPost = ({ post }) => {
+  return (
+    <div className='flex items-center w-full mb-4'>
+      <div className='w-16 flex-none'>
+        <Image
+          src={post.featuredImage.url}
+          alt={post.title}
+          height='60px'
+          width='60px'
+          className='align-middle rounded-full'
+          unoptimized
+        />
+      </div>
+      <div className='flex-grow ml-4'>
+        <p className='text-gray-500 font-xs'>
+          {moment(post.createdAt).format('MMM DD, YYYY')}
+        </p>
+        <Link href={`post/${post.slug}`} className='text-md'>
+          {post.title}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedPost;
