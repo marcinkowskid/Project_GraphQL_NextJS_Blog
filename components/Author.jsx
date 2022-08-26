60320d1b34ef1cc293fbf61d351e4b6a0c0109fb
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-08-26 10:53:01 +0200

    Author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,5 +1,22 @@
-const Author = () => {
-  return <div>Author</div>;
+import Image from 'next/image';
+
+const Author = ({ author }) => {
+  return (
+    <div className='text-center relative rounded-lg bg-black bg-opacity-20 mt-20 mb-8 p-12'>
+      <div className='absolute left-0 right-0 -top-14'>
+        <Image
+          src={author.photo.url}
+          alt={author.name}
+          height='100px'
+          width='100px'
+          className='align-middle rounded-full'
+          unoptimized
+        />
+      </div>
+      <h3 className='text-white mt-4 mb-4 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
+    </div>
+  );
 };
 
 export default Author;
